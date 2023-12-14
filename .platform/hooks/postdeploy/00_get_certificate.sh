442775c47a46d35e43922e09048471a0baa17e14
amitchelll <131500645+amitchelll@users.noreply.github.com>
2023-12-14 09:42:23 -0700

    security

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo certbot -n -d weeWear.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email pickettelise0@gmail.com
+# Place in .platform/hooks/postdeploy directory
+sudo certbot -n -d YOURDOMAINHERE --nginx --agree-tos --email YOUREMAILHERE
